42863531858094aae003221b5e337073f93e95b1
Francisco Adao Eloy Junior <franciscoadaoeloyjunior@MacBook-Air-de-Francisco-5.local>
2020-10-18 16:33:33 -0300

    bug and security fix

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,14 @@
 from flask import Flask
 from app.routes import init_routes
+from flask_wtf.csrf import CSRFProtect
 
 def create_app():
 
     app = Flask(__name__)
     init_routes(app)
+
+    csrf = CSRFProtect()
+    csrf.init_app(app)
 
     return app
 
